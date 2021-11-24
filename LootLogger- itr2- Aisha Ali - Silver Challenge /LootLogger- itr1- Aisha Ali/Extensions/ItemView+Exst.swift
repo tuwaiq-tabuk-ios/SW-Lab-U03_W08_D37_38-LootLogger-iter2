49a1a3314b320dbff1d719ad094753c1cc2613b4
Aisha Ali <aamiss1496@gmail.com>
2021-11-24 09:00:04 +0300

    LootLogger Itr2

---
+++ LootLogger- itr2- Aisha Ali - Silver Challenge /LootLogger- itr1- Aisha Ali/Extensions/ItemView+Exst.swift
@@ -0,0 +1,17 @@
+//
+//  TableView+Exst.swift
+//  LootLogger- itr1- Aisha Ali
+//
+//  Created by Aisha Ali on 11/23/21.
+//
+
+import Foundation
+import UIKit
+
+extension ItemsViewController {
+  
+  func overlapping (_ name:UILabel){
+    name.adjustsFontSizeToFitWidth = true
+    name.minimumScaleFactor = 0.5
+  }
+}
