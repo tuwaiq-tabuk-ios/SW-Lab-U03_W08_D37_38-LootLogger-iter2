de44442c721170d53c8d1b5d146caf3275c25f81
عبدالعزيز البلوي <bdalzyzalblwy@MacBook-Air-bdalzyz.local>
2021-11-24 10:06:36 +0300

    finish

---
+++ LootLoggers-Abdulaziz-albalawi-2-Bronze Challenge/LootLoggers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLoggers
+//
+//  Created by عبدالعزيز البلوي on 12/04/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
