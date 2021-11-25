eec635af0a15ff9cb97465af91c8f5edf1dbfff0
HAJAR <hajar@HAJARs-MacBook-Pro.local>
2021-11-25 09:04:03 +0300

    Completed LootLogger-iter2

---
+++ LootLogger-ch10-HajarAlatawi/LootLogger-Bronze:Silver-HajarAlatawi/LootLogger/Model/ ItemCell.swift
@@ -0,0 +1,11 @@
+
+//LootLogger-Bronze/Silver-HajarAlatawi
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+  
+  @IBOutlet var nameLabel: UILabel!
+  @IBOutlet var serialNumberLabel: UILabel!
+  @IBOutlet var valueLabel: UILabel!
+}
